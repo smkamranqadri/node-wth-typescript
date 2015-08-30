4c21442a0408f9a0a08ed9d2a6f445f28af60fb2
Muhammad Kamran <smkamranqadri@yahoo.com>
2015-08-30 22:51:01 +0500

    NodsJS via Typesciprt - Http server via modules & class

---
+++ 9-hello-world-via-class/lib/HttpTS.js
@@ -0,0 +1,60 @@
+///<reference path="../typings/node/node.d.ts" />
+var http = require('http');
+var fs = require('fs');
+var HttpTS = (function () {
+    function HttpTS(port) {
+        this._port = port || 3000;
+        this._routes = [];
+    }
+    HttpTS.prototype.start = function () {
+        this._server = http.createServer().listen(3000);
+        console.log('Server running at http://localhost:3000/');
+        this._server.on('request', function (req, res) {
+            //handle routes
+            console.log(req.url);
+            for (var i = 0; i < this._routes.length; i++) {
+                var route = this._routes[i];
+                console.log(route);
+                if (route.url == req.url) {
+                    console.log('FOUND URL');
+                    if (route.type == 'text') {
+                        console.log('FOUND TEXT');
+                        this._send(route.response, res);
+                    }
+                    else if (route.type == 'html') {
+                        console.log('FOUND FILE');
+                        this._sendFile(route.response, res);
+                    }
+                    break;
+                }
+            }
+            this._errorHandler(res);
+        });
+    };
+    HttpTS.prototype.route = function (url, type, response) {
+        this._routes.push({ url: url, type: type, response: response });
+    };
+    HttpTS.prototype._errorHandler = function (res, err) {
+        if (err) {
+            console.log(err.toString);
+        }
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        this._sendFile('../html/error.html', res);
+        return;
+    };
+    HttpTS.prototype._Send = function (Text, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(Text);
+    };
+    HttpTS.prototype._sendFile = function (path, res) {
+        fs.readFile(path, function (err, data) {
+            if (err) {
+                this._errorHandler(res, err);
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data.toString);
+        });
+    };
+    return HttpTS;
+})();
+exports.default = HttpTS;
---
+++ 9-hello-world-via-class/lib/HttpTS.ts
@@ -0,0 +1,76 @@
+///<reference path="../typings/node/node.d.ts" />
+
+import http = require('http')
+import fs   = require('fs')
+
+interface Iroute {
+  url : string, 
+  type : string, 
+  response : string
+}
+
+class HttpTS {
+
+  private _server : http.Server
+  private _port   : number
+  private _routes : Array<Iroute>
+  
+  constructor (port? : number){
+    this._port = port || 3000;
+	  this._routes = [];
+  }
+  
+  start () {
+    this._server = http.createServer().listen(3000);
+    console.log('Server running at http://localhost:3000/');
+    this._server.on('request', function(req : http.ServerRequest, res : http.ServerResponse){
+      //handle routes
+	  console.log(req.url);
+      for (let i = 0; i < this._routes.length; i++) {                                         //not able us this
+        let route : Iroute = this._routes[i];                        
+		console.log(route);
+        if (route.url == req.url) {
+			console.log('FOUND URL');
+          if (route.type == 'text') {
+			  console.log('FOUND TEXT');
+              this._send(route.response, res)
+          } else if (route.type == 'html') {
+			  console.log('FOUND FILE');
+              this._sendFile(route.response, res)
+          }
+          break;
+        }
+      }
+      this._errorHandler(res)
+    })
+  }
+  
+  route (url : string, type : string, response : string) {
+    this._routes.push({url, type, response});
+  }
+  
+  private _errorHandler (res : http.ServerResponse, err? : Error) {
+    if (err) {console.log(err.toString)}
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    this._sendFile('../html/error.html', res);
+    return;
+  }
+  
+  private _Send (Text : string, res : http.ServerResponse) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(Text);
+  }
+  
+  private _sendFile (path : string, res : http.ServerResponse) {
+    fs.readFile(path, function(err : Error, data : Buffer) {
+      if (err) {
+        this._errorHandler(res,err)
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data.toString);
+    })
+  }
+
+}
+
+export default HttpTS;
